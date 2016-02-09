0147fc238f34e87133c3ae0c8b3b2add1dc2b23b
przemyslaw.piotrowski <przemyslaw.piotrowski@adform.com>
2016-02-09 23:11:46 +0100

    Api project

---
+++ api/src/main/scala/com/example/api/Main.scala
@@ -0,0 +1,10 @@
+package com.example.api
+
+import com.example.common.Resource
+
+object Main extends App {
+  val resource = Resource("foo")
+  println("Serving resources....")
+  Thread.sleep(3*1000)
+  println("API terminated...")
+}
